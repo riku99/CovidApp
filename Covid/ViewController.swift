bef8d827c49823d1e84f32fd89894e7ac08cb879
riku <riku.suny@gmail.com>
2022-06-29 22:29:17 +0900

    Initial Commit

---
+++ Covid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Covid
+//
+//  Created by 黒澤陸 on 2022/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
